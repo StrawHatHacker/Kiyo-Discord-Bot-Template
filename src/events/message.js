e042c5a734e0bc13d908351238a18e4a5873f5da
strawhathacker <ntahsp@gmail.com>
2021-02-22 14:42:30 +0200

    Moved things around

--- src/events/message.js
+++ src/events/message.js
@@ -1,19 +1,11 @@
-const handleError = (e, m) => {
-    // 404 Usually happens when member.fetch,
-    if (e.httpStatus === 404) m.channel.send('Didn\'t find what you were looking for');
-    // 403 Usually happens when channel.send,
-    // BE CAREFUL, you might not see errors in your console because of this line
-    else if (e.httpStatus === 403) return;
-    // All other errors
-    else {
-        console.log(e)
-        m.channel.send('Unknown Error');
-    }
-};
+const utils = require('../utils/database');
+const GuildModel = require('../models/guild');
 
-module.exports = (Kiyo, message) => {
+module.exports = async (Kiyo, message) => {
     if (message.author.bot || message.channel.type === 'dm') return;
     if (!message.content.toLowerCase().startsWith(Kiyo.globalPrefix)) return;
+
+    const Guild = await utils.guild.findOneOrCreate(GuildModel, [message.guild.id]);
 
     const [cmd, ...args] = message.content
         .toLowerCase()
@@ -23,6 +15,19 @@ module.exports = (Kiyo, message) => {
     for (const [commandName, command] of Kiyo.commands) {
         if (commandName !== cmd) continue;
 
-        command.run({ message, Kiyo, args }).catch(e => handleError(e, message));
-    };
+        command.run({ message, Kiyo, args }).catch(e => errotHandler(e, message));
+    }
+};
+
+const errotHandler = (e, m) => {
+    // 404 Usually happens when member.fetch,
+    if (e.httpStatus === 404) m.channel.send('Didn\'t find what you were looking for');
+    // 403 Usually happens when channel.send,
+    // BE CAREFUL, you might not see errors in your console because of this line
+    else if (e.httpStatus === 403) return;
+    // All other errors
+    else {
+        console.log(e);
+        m.channel.send('Unknown Error');
+    }
 };
