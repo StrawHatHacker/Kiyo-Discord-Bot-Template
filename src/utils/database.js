98413a04423c5280327eb54b0b765ce0fecb418f
strawhathacker <ntahsp@gmail.com>
2021-02-22 14:42:48 +0200

    New utils functions

---
+++ src/utils/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+    guild: {
+        async findOneOrCreate(GuildModel, [id]) {
+            let g = await GuildModel.findById(id);
+            if (g === null) {
+                g = await GuildModel.create({
+                    _id: id
+                });
+            }
+            return g;
+        },
+    },
+};
