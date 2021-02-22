6d01ccb2cedbd22c0f0a055a5b5b21156bb12c3e
strawhathacker <ntahsp@gmail.com>
2021-02-22 14:40:10 +0200

    Moved Bot to its own class file

--- src/classes/Bot.js
+++ src/classes/Bot.js
@@ -1,3 +1,32 @@
+const { Client, Collection } = require('discord.js');
+const readFiles = require('readdirp');
+const mongoose = require('mongoose');
+
+module.exports = class Bot extends Client {
+    constructor(config) {
+        super(config);
+        this.commands = new Collection();
+        this.globalPrefix = '.';
+    }
+
+    async _loadCommands() {
+        for await (const file of readFiles('./commands')) {
+            const command = require(file.fullPath);
+            this.commands.set(command.name, command);
+        }
+        return this;
+    }
+
+    async _loadEvents() {
+        for await (const file of readFiles('./events')) {
+            const event = require(file.fullPath);
+            const eventName = file.path.split('.')[0];
+
+            this.on(eventName, event.bind(null, this));
+        }
+        return this;
+    }
+
     async _connectToDB() {
         await mongoose.connect(process.env.DB_CONNECTION_STRING, {
             useNewUrlParser: true,
@@ -6,3 +35,10 @@
         console.log('Connected to DB');
     }
 
+    async start() {
+        await this._loadCommands();
+        await this._loadEvents();
+        await this._connectToDB();
+        await this.login(process.env.DISCORD_BOT_TOKEN);
+    }
+};
--- src/index.js
+++ src/index.js
@@ -1,38 +1,5 @@
-const Discord = require('discord.js');
-const readFiles = require('readdirp');
 require('dotenv').config();
-
-class Bot extends Discord.Client {
-    constructor(config) {
-        super(config);
-        this.commands = new Discord.Collection();
-        this.globalPrefix = '.';
-    };
-
-    async loadCommands() {
-        for await (const file of readFiles('./commands')) {
-            const command = require(file.fullPath);
-            this.commands.set(command.name, command);
-        };
-        return this;
-    };
-
-    async loadEvents() {
-        for await (const file of readFiles('./events')) {
-            const event = require(file.fullPath);
-            const eventName = file.path.split('.')[0];
-    
-            this.on(eventName, event.bind(null, this));
-        };
-        return this;
-    };
-
-    async start() {
-        await this.loadCommands();
-        await this.loadEvents();
-        await this.login(process.env.DISCORD_BOT_TOKEN);
-    };
-};
+const Bot = require('./classes/Bot');
 
 const Kiyo = new Bot({
     presence: {
