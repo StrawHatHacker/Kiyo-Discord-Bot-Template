bcce7ff94234f5ae2f9f604063904601befc6adb
strawhathacker <ntahsp@gmail.com>
2021-02-23 16:39:30 +0200

    Improved the ping command

--- src/commands/info/ping.js
+++ src/commands/info/ping.js
@@ -1,25 +1,13 @@
 'use strict';
 
-const Embed = require('../../classes/Embed');
-
 module.exports = {
     name: 'ping',
     description: 'Pings the bot',
     async run({ message, Kiyo }) {
-        try {
-            const botMessage = await message.channel.send('Pinging...');
-
-            const embed = new Embed()
-                .setAuthor('Kiyo Bot', Kiyo.user.displayAvatarURL({ size: 64 }))
-                .addField('Ping', botMessage.createdTimestamp - message.createdTimestamp + ' ms')
-                .addField('Serving', `${Kiyo.guilds.cache.size} servers`)
-                .setFooter('Made by Skillers3', Kiyo.user.displayAvatarURL({ size: 64 }));
-
-            botMessage.edit('', embed);
-
-        } catch (error) {
-            console.log(error);
-            message.channel.send('There was an error').catch(() => { });
-        }
+        // Sending temporary message
+        const botMessage = await message.channel.send('Pinging...');
+        
+        // Calculating ping, it's the time between the user message and our botMessage
+        botMessage.edit(`Ping: ${botMessage.createdTimestamp - message.createdTimestamp}ms\nWebSocket: ${Kiyo.ws.ping}ms`);
     }
 };
