96fb2639af22c236612aaf618adcc75415185120
strawhathacker <ntahsp@gmail.com>
2021-03-06 12:36:09 +0200

    Removed unneeded destructuring

--- src/utils/database.js
+++ src/utils/database.js
@@ -2,7 +2,7 @@
 
 module.exports = {
     guild: {
-        async findOneOrCreate(GuildModel, [id]) {
+        async findOneOrCreate(GuildModel, id) {
             let g = await GuildModel.findById(id);
             if (g === null) {
                 g = await GuildModel.create({
