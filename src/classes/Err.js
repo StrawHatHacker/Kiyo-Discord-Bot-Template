74ffbbe19d37840e7d34722406654b2c2521b85d
strawhathacker <ntahsp@gmail.com>
2021-03-26 13:22:37 +0200

    New error methods

--- src/classes/Err.js
+++ src/classes/Err.js
@@ -10,13 +10,43 @@ module.exports = class Err {
         this.httpStatus = httpStatus ?? 500;
     }
 
+    setName(name) {
+        this.name = name;
+        return this;
+    }
+
+    setMessage(message) {
+        this.message = message;
+        return this;
+    }
+
     inputErr() {
         this.name = 'Input Error';
         return this;
     }
 
+    timeErr() {
+        this.name = 'Timed Out';
+        return this;
+    }
+
+    syntaxErr() {
+        this.name = 'Syntax Error';
+        return this;
+    }
+
     memberNotFound() {
         this.message = 'Member not found';
+        return this;
+    }
+
+    jsonNotValid() {
+        this.message = 'JSON not valid';
+        return this;
+    }
+
+    timedOut() {
+        this.message = 'User took too long to respond';
         return this;
     }
 };
