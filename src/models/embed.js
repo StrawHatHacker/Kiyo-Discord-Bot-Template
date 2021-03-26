252fc888d82b5d32823f1e64ed0b815e2a2f3ebc
strawhathacker <ntahsp@gmail.com>
2021-03-26 13:29:05 +0200

    Embed mongoose schema

---
+++ src/models/embed.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const { Schema, model } = require('mongoose');
+
+const EmbedSchema = new Schema({
+    guildId: String,
+    content: {
+        type: String,
+        default: ''
+    },
+    title: String,
+    description: String,
+    url: String,
+    color: Number,
+    timestamp: Number,
+    fields: [{
+        name: String,
+        value: String,
+        inline: Boolean
+    }],
+    thumbnail: {
+        url: String
+    },
+    image: {
+        url: String
+    },
+    author: {
+        name: String,
+        iconURL: String,
+        url: String
+    },
+    footer: {
+        text: String,
+        iconURL: String
+    }
+}, {
+    timestamps: true,
+    validateBeforeSave: true,
+    minimize: false,
+    id: false,
+    collection: 'embed',
+    autoIndex: false
+});
+
+module.exports = model('embed', EmbedSchema);
