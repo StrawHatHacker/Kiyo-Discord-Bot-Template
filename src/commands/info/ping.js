8daf4c147883d444e596c4ae096c266787eefe60
Leo <72186159+disaaalt@users.noreply.github.com>
2021-05-10 21:18:51 +1000

    Add more stuff to ping to make it look cooler

--- src/commands/info/ping.js
+++ src/commands/info/ping.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const Embed = require('../../classes/Embed');
+
 module.exports = {
     name: 'Ping',
     description: 'Pings the bot',
@@ -13,7 +15,21 @@ module.exports = {
         // Sending temporary message
         const botMessage = await message.channel.send('Pinging...');
         
-        // Calculating ping, it's the time between the user message and our botMessage
-        botMessage.edit(`Ping: ${botMessage.createdTimestamp - message.createdTimestamp}ms\nWebSocket: ${client.ws.ping}ms`);
+        // The times for the embed - too lazy to use inline
+        const days = Math.floor(bot.uptime / 86400000);
+        const hours = Math.floor(bot.uptime / 3600000) % 24;
+        const minutes = Math.floor(bot.uptime / 60000) % 60;
+        const ping = Date.now() - message.createdTimestamp;
+        
+        // Lets construct the embed!
+        // Im using the Embed class to make the embed - thanks to the creator.
+        const pingembed = new Embed()
+            .setTitle('Pong!')
+            .addField('Ping', ':hourglass_flowing_sand: `' + (botMessage.createdTimestamp - message.createdTimestamp) + 'ms`', true)
+            .addField('Uptime', ':clock1: `' + days + 'd, ' + hours + 'h`', true)
+            .addField('Memory Usage', ':dna: `' + Math.trunc(process.memoryUsage().heapUsed / 1024 / 1024) + 'mb`', true)
+           
+        // Sending the embed
+        botMessage.edit({ embed: pingembed });
     }
 };
