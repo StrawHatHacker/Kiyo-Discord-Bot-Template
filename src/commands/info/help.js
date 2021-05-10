8f31efa098991a28e1fe58ee5512c5471549e9c8
strawhathacker <ntahsp@gmail.com>
2021-05-10 18:40:33 +0300

    Massive changes for help command

--- src/commands/info/help.js
+++ src/commands/info/help.js
@@ -5,7 +5,7 @@ const Embed = require('../../classes/Embed');
 
 module.exports = {
     name: 'Help',
-    description: 'Show all commands and information about the bot, or information about an individual command.',
+    description: 'Show all commands and information about the bot, or information about an individual command or module.',
     aliases: ['help'],
     syntax: 'help [command name]',
     requiredPermissions: {
@@ -13,66 +13,51 @@ module.exports = {
         client: []
     },
     async run({ client, message, args, Guild }) {
+        // If no argument is passed (EG. .help)
         if (args.length === 0) {
-
-            let fields = {};
-
-            for (let cmd of client.commands) {
-                if (!fields[cmd.module]) fields[cmd.module] = [];
-                fields[cmd.module].push(cmd);
-            }
-
-            const embed = new Embed()
+            return message.channel.send(new Embed()
+                .setColor(message.guild.me.roles.color?.hexColor || 0xffffff)
                 .setThumbnail(client.user.displayAvatarURL({ dynamic: true, size: 2048 }))
                 .addDescription('[ɪɴᴠɪᴛᴇ ᴍᴇ](https://discord.com/api/oauth2/authorize?client_id=794989015765483571&permissions=0&scope=bot) ┃ [ꜱᴜᴘᴘᴏʀᴛ ꜱᴇʀᴠᴇʀ](https://discord.gg/ypEBGHB) \n')
-                .addDescription(`Hey! I'm ${client.user.username} and I'm Open Source. You can find me [here](https://github.com/StrawHatHacker/Kiyo-Discord-Bot).\n\u200B`);
-
-            for (const key of Object.keys(fields)) {
-                if (key === 'Reactions') {
-                    embed.addField(key, fields[key][0].aliases.map(a => `\`${a}\``).join(' '));
-                } else {
-                    embed.addField(key, fields[key].map(f => `\`${f.name}\``).join(' '));
-                }
-            }
-
-            embed.addField('\u200B', '\u200B');
-            embed.addField('Referrals', 'Hosting Services: [DigitalOcean](https://m.do.co/c/f51cd516e684), [Hetzner](https://hetzner.cloud/?ref=0fswF9Kv99Av)\n\
-CDN: [BunnyCDN](https://bunnycdn.com/?ref=1dqicv581x)\n\
-VPN: [Surfshark](https://surfshark.club/friend/VZJP3rPr)');
-            embed.addField('Donate', '[Ko-fi](https://ko-fi.com/skillers3)');
-
-            message.channel.send(embed);
-
-        } else {
+                .addDescription(`Hey! I'm ${client.user.username} and I'm Open Source. You can find me [here](https://github.com/StrawHatHacker/Kiyo-Discord-Bot).\n\u200B`)
+                .addDescription(`Type \`${Guild.prefix}help moduleName\` to see all commands of that module.`)
+                .addDescription(`Type \`${Guild.prefix}help commandName\` to see all information of that command.`)
+                .addField('Modules', Object.keys(client.modulesWithCommands).map(m => `\`${m}\``).join(', '))
+                .addField('Referrals', 'Hosting Services: [DigitalOcean](https://m.do.co/c/f51cd516e684), [Hetzner](https://hetzner.cloud/?ref=0fswF9Kv99Av)\nCDN: [BunnyCDN](https://bunnycdn.com/?ref=1dqicv581x)\nVPN: [Surfshark](https://surfshark.club/friend/VZJP3rPr)')
+                .addField('Donate', '[Ko-fi](https://ko-fi.com/skillers3)'));
+        }
+        // Else, meaning there is an argument
+        else {
             const arg = args[0].toLowerCase();
 
-            // TODO optimize this / make it look cleaner
-            for (let cmd of client.commands) {
-                if (arg === cmd.name || cmd.aliases.includes(arg)) {
-                    return message.channel.send(new Embed()
-                        .addDescription(cmd.description + '\n\u200B')
-                        .addField('Syntax', '`' + Guild.prefix + cmd.syntax + '`')
-                        .addField('Aliases', cmd.aliases.map(a => `\`${a}\``).join(', '))
-                        .addField('Module', cmd.module)
-                        .addField('User Permissions', cmd.requiredPermissions.user.length === 0 ? '`None`' : new Perms(cmd.requiredPermissions.user).formatToReadableCode())
-                        .addField('Bot Permissions', cmd.requiredPermissions.client.length === 0 ? '`None`' : new Perms(cmd.requiredPermissions.client).formatToReadableCode())
-                    );
-                } else if (arg === cmd.module.toLowerCase()) {
-                    if (cmd.module === 'Reactions') {
-                        return message.channel.send(new Embed()
-                            .setTitle('Module: Reactions')
-                            .addField('Commands', cmd.aliases.map(a => `\`${a}\``).join(', ')));
-                    } else {
-                        let fields = {};
+            // Search throught modules
+            if (Object.keys(client.modulesWithCommands).includes(arg)) {
+                let moduleCommands;
 
-                        for (let cmd of client.commands) {
-                            if (!fields[cmd.module]) fields[cmd.module] = [];
-                            fields[cmd.module].push(cmd);
-                        }
+                if (arg === 'reactions') moduleCommands = client.modulesWithCommands['reactions'][0].aliases;
+                else moduleCommands = client.modulesWithCommands[arg].map(c => c.name);
 
+                return message.channel.send(new Embed()
+                    .setColor(message.guild.me.roles.color?.hexColor || 0xffffff)
+                    .setTitle(`Module: ${arg}`)
+                    .addField('Commands', moduleCommands.map(c => `\`${c}\``).join(', '))
+                );
+
+            }
+            // Search throught command names and aliases
+            else {
+                for (let cmd of client.commands) {
+                    if (arg === cmd.name || cmd.aliases.includes(arg)) {
                         return message.channel.send(new Embed()
-                            .setTitle(`Module: ${cmd.module}`)
-                            .addField('Commands', fields[cmd.module].map(c => `\`${c.name}\``).join(', ')));
+                            .setColor(message.guild.me.roles.color?.hexColor || 0xffffff)
+                            .setTitle(`Command: ${cmd.name}`)
+                            .addDescription(`Syntax: \`${Guild.prefix}${cmd.syntax}\``)
+                            .addDescription(`Aliases: ${cmd.aliases.map(a => `\`${a}\``).join(', ')}`)
+                            .addDescription(`Module: ${cmd.module}`)
+                            .addDescription(`User Permissions: ${cmd.requiredPermissions.user.length === 0 ? '`None`' : new Perms(cmd.requiredPermissions.user).formatToReadableCode()}`)
+                            .addDescription(`Bot Permissions: ${cmd.requiredPermissions.client.length === 0 ? '`None`' : new Perms(cmd.requiredPermissions.client).formatToReadableCode()}`)
+                            .addDescription('\n' + cmd.description)
+                        );
                     }
                 }
             }
