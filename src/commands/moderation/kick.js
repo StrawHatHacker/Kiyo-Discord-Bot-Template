f06a4a01f934b71c93b0e794d11d73168d56b12f
strawhathacker <ntahsp@gmail.com>
2021-09-19 12:54:01 +0300

    🆕 Kick command

---
+++ src/commands/moderation/kick.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const safeFindMember = require('../../utils/safeFindMember');
+const isModerator = require('../../utils/isModerator');
+const Embed = require('../../classes/Embed');
+const Err = require('../../classes/Err');
+
+module.exports = {
+    name: 'kick',
+    description: 'Kick a member from the server',
+    aliases: [],
+    syntax: 'kick <member> [reason]',
+    requiredPermissions: {
+        user: ['BAN_MEMBERS'],
+        client: ['BAN_MEMBERS']
+    },
+    async run({ message, args }) {
+        const memberInput = args[0];
+        if (!memberInput) return;
+
+        const memberToKick = await safeFindMember(message, memberInput);
+        if (!memberToKick) throw new Err().inputErr().memberNotFound();
+
+        if (!memberToKick.kickable) throw new Err(400).inputErr().setMessage('Member is higher on the role hierarchy');
+        if (isModerator(memberToKick)) throw new Err(400).inputErr().setMessage('Member has moderation permissions');
+
+        const reason = args.length > 1 ? args.slice(1).join(' ') : 'No reason provided';
+
+        await memberToKick.send(`You have been kicked from **${message.guild.name}** for: *${reason}*`).catch(() => null);
+        await memberToKick.kick(reason);
+
+        const e = new Embed().setDescription(`**${memberToKick.user.tag} has been kicked**`).isSuccess();
+        await message.channel.send({ embeds: [e] });
+    }
+};
