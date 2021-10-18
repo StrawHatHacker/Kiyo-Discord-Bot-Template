3684112480c515b7bfa9113c82d826cc22b2fb14
strawhathacker <ntahsp@gmail.com>
2021-10-18 10:37:05 +0300

    🆕 Prefix Command

---
+++ src/commands/moderation/set/prefix.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const Err = require('../../../classes/Err');
+
+module.exports = {
+    name: 'prefix',
+    description: 'Change the bot\'s prefix',
+    aliases: [],
+    syntax: 'prefix <new prefix>',
+    requiredPermissions: {
+        user: ['ADMINISTRATOR'],
+        client: []
+    },
+    async run({ message, args, Guild }) {
+        let prefixInput = args[0];
+        if (!prefixInput) return;
+
+        if (prefixInput.length > 3) throw new Err(400).inputErr().setMessage('The prefix must be 3 characters or less.');
+
+        await Guild.updateOne({
+            prefix: prefixInput
+        });
+
+        await message.channel.send(`Prefix changed to \`${prefixInput}\``);
+    }
+};
