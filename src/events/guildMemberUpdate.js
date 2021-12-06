4f2fa022aaf709783d6ae6876cb5ec3035a36c0a
strawhathacker <ntahsp@gmail.com>
2021-12-06 17:28:15 +0200

    GuildMemberUpdate log

---
+++ src/events/guildMemberUpdate.js
@@ -0,0 +1,9 @@
+'use strict';
+
+// const GuildModel = require('../models/guild');
+// const sendLog = require('../utils/sendLog');
+
+// eslint-disable-next-line no-unused-vars
+module.exports = async (_client, oldMember, newMember) => {
+    // Not working as intended as of 6/12/2021
+};
