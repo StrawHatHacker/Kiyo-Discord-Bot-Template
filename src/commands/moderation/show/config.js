6356a3ae08f337ad70b49850a4655659750179b2
strawhathacker <ntahsp@gmail.com>
2022-06-02 15:43:03 +0300

    🆕 Config Command

---
+++ src/commands/moderation/show/config.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const Embed = require('../../../classes/Embed');
+
+const spacing = '\u2005\u2005\u2005\u2005\u2005\u2005';
+
+module.exports = {
+    name: 'showconfig',
+    description: 'View the current configuration of the the server',
+    aliases: [],
+    syntax: 'showconfig',
+    requiredPermissions: {
+        user: ['ADMINISTRATOR'],
+        client: []
+    },
+    async run({ message, Guild }) {
+
+        const e = new Embed()
+            .setAuthor({ name: `Server Config for ${message.guild.name}`, iconURL: message.guild.iconURL({ size: 512 }) });
+
+        e.addDescription(`${Guild.features.moderationlog ? '✅' : '❎'} Moderation Log`);
+        e.addDescription(`${spacing}${Guild.moderation_log_channel_id ? `<#${Guild.moderation_log_channel_id}>` : '`No channel set`'}`);
+
+        e.addDescription(`${Guild.features.messagelog ? '✅' : '❎'} Message Log`);
+        e.addDescription(`${spacing}${Guild.message_log_channel_id ? `<#${Guild.message_log_channel_id}>` : '`No channel set`'}`);
+
+        e.addDescription(`${Guild.features.serverlog ? '✅' : '❎'} Server Log`);
+        e.addDescription(`${spacing}${Guild.server_log_channel_id ? `<#${Guild.server_log_channel_id}>` : '`No channel set`'}`);
+
+        e.addDescription(`${Guild.features.memberlog ? '✅' : '❎'} Member Log`);
+        e.addDescription(`${spacing}${Guild.member_log_channel_id ? `<#${Guild.server_log_channel_id}>` : '`No channel set`'}`);
+
+        e.addDescription(`${Guild.features.welcomemessages ? '✅' : '❎'} Welcome Messages`);
+        e.addDescription(`${spacing}${Guild.welcome_channel_id ? `<#${Guild.welcome_channel_id}>` : '`No channel set`'}`);
+
+        e.addDescription(`${Guild.features.leavemessages ? '✅' : '❎'} Leave Messages`);
+        e.addDescription(`${spacing}${Guild.leave_channel_id ? `<#${Guild.leave_channel_id}>` : '`No channel set`'}`);
+
+        e.addDescription(`\n${'✅'} - enabled | ${'❎'} - disabled`);
+
+        await message.channel.send({ embeds: [e] });
+    }
+};
