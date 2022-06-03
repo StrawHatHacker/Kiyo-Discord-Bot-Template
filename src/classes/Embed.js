0d1657ad104be3004b92a59d35107ee95997d1b9
strawhathacker <ntahsp@gmail.com>
2022-06-03 13:37:57 +0300

    Embed Class Update

--- src/classes/Embed.js
+++ src/classes/Embed.js
@@ -13,11 +13,22 @@ module.exports = class Embed extends MessageEmbed {
         if (data.timestamp && typeof data.timestamp !== 'number') throw new Err(400).inputErr().setMessage('Timestamp should be a number');
     }
 
-    // Appends a string to the `.description` property of the object (don't confuse with the inherited `.setDescription` method which replaces the previous description value)
+    // TODO check here if embed description length is above the character limit
+    // Appends a string to the `.description` property of the object **IN A NEW LINE** (don't confuse with the inherited `.setDescription` method which replaces the previous description value)
     addDescription(appendable) {
         /*                 `.description` property starts as null                                */
         /*                 if embed has null description initialize it as an empty string        */
         this.description = (this.description === null ? '' : this.description) + '\n' + appendable;
+        return this;
+    }
+
+    // TODO check here if embed description length is above the character limit
+    // Appends a string to the `.description` property of the object **NOT IN A NEW LINE** (don't confuse with the inherited `.setDescription` method which replaces the previous description value)
+    appendDescription(appendable) {
+        console.log(appendable);
+        /*                 `.description` property starts as null                                */
+        /*                 if embed has null description initialize it as an empty string        */
+        this.description = (this.description === null ? '' : this.description) + appendable.trim();
         return this;
     }
 
