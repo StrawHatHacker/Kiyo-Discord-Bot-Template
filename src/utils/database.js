f150f65cc9a98e0b3f5537f6b871908362115178
strawhathacker <ntahsp@gmail.com>
2022-06-17 17:45:10 +0300

    Code improvements

--- src/utils/database.js
+++ src/utils/database.js
@@ -7,18 +7,18 @@ module.exports = {
     guild: {
         async findOneOrCreate(id) {
             let g = await GuildModel.findById(id);
-            if (g === null) {
-                g = await GuildModel.create({ _id: id });
-            }
+
+            if (!g) g = await GuildModel.create({ _id: id });
+
             return g;
         }
     },
     user: {
         async findOneOrCreate(id) {
             let u = await UserModel.findById(id);
-            if (u === null) {
-                u = await UserModel.create({ _id: id });
-            }
+
+            if (!u) u = await UserModel.create({ _id: id });
+
             return u;
         }
     }
