a3b83e40812be78e5509fd6263616ab7f4ad7f78
strawhathacker <ntahsp@gmail.com>
2023-01-18 12:40:06 +0200

    Fix Userinfo bug

--- src/slashCommands/Info/userinfo.js
+++ src/slashCommands/Info/userinfo.js
@@ -54,11 +54,13 @@ module.exports = {
         if (interaction.guild.features.includes('MEMBER_VERIFICATION_GATE_ENABLED'))
             e.addDescription(`Passed Screening: ${targetMember.pending}`);
 
-        e.addDescription(`Joined at: ${joinedAt}`)
-            .addDescription(`Created at: ${createdAt}`)
-            .addField(`Roles (${targetMember.roles.cache.size - 1})`, `Highest: ${targetMember.roles.highest.toString()} \n\n ${rolesString}`)
-            .addField('Key Permissions', keyPerms)
-            .addField('Other Permissions', otherPerms);
+        e.addDescription(`Joined at: ${joinedAt}`).addDescription(`Created at: ${createdAt}`);
+
+        if (rolesString) {
+            e.addField(`Roles (${targetMember.roles.cache.size - 1})`, `Highest: ${targetMember.roles.highest.toString()} \n\n ${rolesString}`);
+        }
+
+        e.addField('Key Permissions', keyPerms).addField('Other Permissions', otherPerms);
 
         await interaction.reply({ embeds: [e] });
     }
--- src/utils/prettifyRoles.js
+++ src/utils/prettifyRoles.js
@@ -3,9 +3,9 @@
 module.exports = roleCollection => {
     // Remove the '@everyone' role from the member and format their roles into a string
     return roleCollection
-        .filter(role => role.name !== '@everyone')
-        .map(role => role.toString())
-        .sort((a, b) => b.rawPoisition - a.rawPoisition)
+        .filter(role => role?.name !== '@everyone')
+        .map(role => role?.toString())
+        .sort((a, b) => b?.rawPoisition - a?.rawPoisition)
         .limit(20)
         .join('')
         .slice(0, 1900);
